d113e63616d8839b0e016f2bcad39fbca524b70b
Ayman Patel <ayman.patel97@outlook.com>
2024-11-10 21:14:30 +0530

    feat: Add Material UI

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
 import type { Preview } from "@storybook/react";
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+// Material UI Font Imports
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
 
 const preview: Preview = {
   parameters: {
@@ -12,3 +20,31 @@ const preview: Preview = {
 };
 
 export default preview;
+
+
+import { createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+export const lightTheme = createTheme({
+    palette: {
+        mode: "light",
+    },
+});
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  })
+]
